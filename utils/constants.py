9ad1580142d94b18484c9ae185ef5b0488a56bcf
lemonsong <yilin19920089@sina.com>
2026-01-27 09:28:08 +0800

    change to absolut path

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,8 @@
 import dotenv
 import os
-PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+PROJECT_PATH = '/Users/yilin/Documents/Projects/stock_analysis'
+#os.path.dirname(os.path.abspath(__file__))
+
 project_dir = os.path.join(os.path.dirname(__file__), os.pardir)
 dotenv_path = os.path.join(project_dir, '.env')
 dotenv.load_dotenv(dotenv_path)
