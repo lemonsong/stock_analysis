fe26d234c732eaaebf0725aee77be0c467b8d74a
lemonsong <yilin19920089@sina.com>
2026-02-25 04:38:18 +0800

    env and dividend

---
+++ _dividend_0extract_ak_dividend.py
@@ -0,0 +1,28 @@
+'''
+Fetch dividends info every half year
+'''
+import os, random, time
+import logging
+logging.basicConfig(
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M",
+    level=logging.INFO, # DEBUG,INFO,WARNING, ERROR, CRITICAL
+)
+import pandas as pd
+import akshare as ak
+from utils.constants import PROJECT_PATH
+
+'''
+https://akshare.akfamily.xyz/data/stock/stock.html#id165
+Manually change the date_input
+'''
+PROGRAM_PATH = f'{PROJECT_PATH}/data/ak_dividend/single_file'
+# TODO: update the date_input
+for date_input in ['20250630', '20240630', '20230630', '20220630', '20210630','20200630',
+                   '20241231', '20231231', '20221231', '20211231','20201231']:
+# date_input = '20251231' # choice of {"XXXX0630", "XXXX1231"}; 从 19901231 开始
+    stock_fhps_em_df = ak.stock_fhps_em(date=date_input)
+    stock_fhps_em_df.to_csv(f'{PROGRAM_PATH}/{date_input}.csv',
+                                                   encoding='utf-8',
+                                                   index=False)
