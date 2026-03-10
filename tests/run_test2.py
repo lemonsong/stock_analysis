8da209783df4556c80f47bb20c4cebccaa8af796
lemonsong <yilin19920089@sina.com>
2026-03-10 14:57:31 +0800

    refactor tests

--- tests/run_test2.py
+++ tests/run_test2.py
@@ -1,8 +1,8 @@
 import sys
-sys.path.append('.')
+sys.path.append('..')
 import pages
 import importlib.util
-spec = importlib.util.spec_from_file_location("buy_signals", "pages/2_💡Buy_Signals.py")
+spec = importlib.util.spec_from_file_location("buy_signals", "../pages/2_💡Buy_Signals.py")
 buy_signals = importlib.util.module_from_spec(spec)
 sys.modules["buy_signals"] = buy_signals
 spec.loader.exec_module(buy_signals)
